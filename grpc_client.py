c84ee662eabfd78b20a832f187662a4c4297aba4
Dasha Fadeeva <genkovna1999@gmail.com>
2026-08-25 15:26:32 +0300

    Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

--- grpc_client.py
+++ grpc_client.py
@@ -7,4 +7,4 @@ channel = grpc.insecure_channel('localhost:50051')
 stub = user_service_pb2_grpc.UserServiceStub(channel)
 
 response = stub.GetUser(user_service_pb2.GetUserRequest(username="Alice"))
-print(response.message)  # Выведет: Привет, Alice!
+print(response.message)
